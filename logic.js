08948b19ba04cbd6a14d95c70b4dda0be860a830
annawae <187255385+knuffelchen@users.noreply.github.com>
2025-08-03 08:48:46 +0200

    console version

---
+++ logic.js
@@ -0,0 +1,39 @@
+function getUserChoice(){
+    let Uchoice = prompt("Enter rock, paper or scissors!");
+    return Uchoice;
+}
+
+function getComputerChoice(){
+    let choice
+    let number = Math.floor(Math.random() * 3);
+    if(number === 0){
+        choice = "rock";
+    }
+    else if(number === 1){
+        choice = "paper";
+    }
+    else{
+        choice = "scissors";
+    }
+    return choice;
+}
+
+let ComputerChoice = getComputerChoice();
+let userChoice = getUserChoice();
+
+console.log(ComputerChoice);
+console.log(userChoice);
+
+function game(){
+    if((ComputerChoice === "rock" && userChoice === "scissors") || (ComputerChoice === "scissors" && userChoice === "paper") || (ComputerChoice === "paper" && userChoice === "rock")){
+        console.log("Computer wins!");
+    }
+    else if((ComputerChoice === "rock" && userChoice === "paper") || (ComputerChoice === "scissors" && userChoice === "rock") || (ComputerChoice === "paper" && userChoice === "scissors")){
+        console.log("User wins!");
+    }
+    else if(ComputerChoice === userChoice){
+        console.log("It's a tie!")
+    }
+}
+
+game();
