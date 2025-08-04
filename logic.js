698b33e40ff2c44fc8f57f905f399eb06d4b323b
annawae <187255385+annawae@users.noreply.github.com>
2025-08-04 22:30:33 +0200

    score added

--- logic.js
+++ logic.js
@@ -27,15 +27,25 @@ console.log(ComputerChoice);
 console.log(userChoice);
 
 function game(){
+    
     if((ComputerChoice === "rock" && userChoice === "scissors") || (ComputerChoice === "scissors" && userChoice === "paper") || (ComputerChoice === "paper" && userChoice === "rock")){
         console.log("Computer wins!");
+        computerScore++;
+        document.getElementById("computerScore").innerHTML =computerScore;
     }
     else if((ComputerChoice === "rock" && userChoice === "paper") || (ComputerChoice === "scissors" && userChoice === "rock") || (ComputerChoice === "paper" && userChoice === "scissors")){
         console.log("User wins!");
+        userScore++;
+        document.getElementById("playerScore").innerHTML = userScore;
     }
     else if(ComputerChoice === userChoice){
         console.log("It's a tie!")
+        computerScore++;
+        userScore++;
+        document.getElementById("playerScore").innerHTML = userScore;
+        document.getElementById("computerScore").innerHTML =computerScore;
     }
+    
 }
 
 
@@ -49,6 +59,9 @@ for(let i=0; i<roundsNum; i++){
     playGame();
 }
 }
+
+let userScore = 0;
+let computerScore = 0;
 
 const startButton = document.getElementById("startButton");
 startButton.addEventListener("click", start);
